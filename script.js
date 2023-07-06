b92621d7c57a175c045a55812f5a2afa4af0cb42
mcthekreator <mcadamsrd@gmail.com>
2023-07-06 13:13:28 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,32 @@
+const dice = document.getElementById("dice");
+const number = document.getElementById("number");
+const advice = document.getElementById("advice");
+
+let numbers = 117;
+
+ dice.addEventListener('click', ()=>{
+
+     
+         numbers++;         
+         number.innerHTML = numbers;
+         advice.innerHTML ='"'+ random +'"';
+ })
+
+  
+ dice.addEventListener('click', ()=>{
+
+     fetch('https://api.quotable.io/random')
+     .then(response => {
+         return response.json ();
+     })
+
+     .then (random => {
+         advice.innerHTML = `${'"'+ random.content + '"'}`;
+         numbers++;
+         number.innerHTML = numbers;
+        
+     })
+    
+    
+ });
+
